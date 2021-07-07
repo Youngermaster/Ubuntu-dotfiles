b114a84e3a49eb17a618b08312a416c2d6b226b3
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-07 08:40:48 -0500

    Themes added to the project

--- startup_installation.sh
+++ startup_installation.sh
@@ -1,5 +1,10 @@
 sudo apt-get update -y
 
+# Creates the folders to work on
+mkdir ~/GitHub/
+mkdir ~/GitHub/Youngermaster/
+mkdir ~/GitHub/AnotherOnes/
+
 # General packages
 sudo apt-get install vim git cmake g++ build-essential curl net-tools screenfetch htop wget -y
 
@@ -25,3 +30,22 @@ sudo apt-get install nodejs -y
 
 # Java Installation
 sudo apt install openjdk-8-jdk -y
+
+# OBS Studio for virtual camera
+sudo apt install ffmpeg -y
+sudo apt install v4l2loopback-dkms -y
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update -y
+sudo apt install obs-studio -y
+
+# Installing Complete Multimedia Support
+sudo apt install ubuntu-restricted-extras -y
+
+# Install GNOME Shell Extensions
+sudo apt-get install chrome-gnome-shell -y
+sudo apt-get install gnome-tweaks -y
+
+# Setup the themes and icons
+mkdir ~/.themes
+mkdir ~/.icons
+
