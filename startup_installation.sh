c55bd69068a47f1e605752233861eccb827e3aba
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-06 22:19:31 -0500

    Startup installation added

---
+++ startup_installation.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y
+sudo apt-get install vim git cmake g++ build-essential curl net-tools screenfetch htop wget -y
+sudo apt-get install python3 python3-pip -y
+sudo apt-get install python3-pyaudio -y
+pip3 install matplotlib numpy pyaudio 
