73a923a04c56a763cdfd103fa56963fae0d2d94c
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-08-04 12:06:31 -0500

    Spotify added to the project

--- startup_installation.sh
+++ startup_installation.sh
@@ -54,3 +54,7 @@ sudo apt-get install virtualbox Virtualbox-dkms -y
 sudo dpkg-reconfigure virtualbox-dkms
 sudo dpkg-reconfigure virtualbox
 
+# Spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update -y && sudo apt-get install spotify-client -y
