3a80ce9fbd8a03ad89c801e8c66a45739a7a83bb
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-11-08 21:37:05 -0500

    Desktop environments separated

---
+++ TWM_based_setup/twm_setup.sh
@@ -0,0 +1,2 @@
+# General Packages
+sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev light -y
