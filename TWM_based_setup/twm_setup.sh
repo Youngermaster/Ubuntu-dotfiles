5b4c5fdc3a69f2764d4f021eb675665bb74ca708
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-11-08 21:44:08 -0500

    BSPWM script updated

--- TWM_based_setup/twm_setup.sh
+++ TWM_based_setup/twm_setup.sh
@@ -1,2 +1,51 @@
 # General Packages
 sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev light -y
+
+# Tiling window manager stuff
+cd ~/GitHub/AnotherOnes/
+git clone https://github.com/baskerville/bspwm.git
+git clone https://github.com/baskerville/sxhkd.git
+cd bspwm/
+make
+sudo make install
+cd ../sxhkd/
+make
+sudo make install
+sudo apt install bspwm -y
+
+# Tiling Window manager Configuration
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+cd ~/GitHub/AnotherOnes/bspwm/
+cp examples/bspwmrc ~/.config/bspwm/
+chmod +x ~/.config/bspwm/bspwmrc 
+cp examples/sxhkdrc ~/.config/sxhkd/
+
+mkdir ~/.config/bspwm/scripts/
+touch ~/.config/bspwm/scripts/bspwm_resize; chmod +x ~/.config/bspwm/scripts/bspwm_resize
+
+# Polybar installation
+sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y 
+ 
+cd ~/GitHub/AnotherOnes
+git clone --recursive https://github.com/polybar/polybar
+cd polybar/
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+
+# Picom Installation
+sudo apt update -y
+sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y
+ 
+git clone https://github.com/ibhagwan/picom.git
+cd picom/
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
+# Rofi installation
+sudo apt install rofi -y
