daff2e4b166476977927807c5d05abee2ef56abd
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-04-15 23:18:24 -0500

    🐋 Docker support added

--- startup_installation.sh
+++ startup_installation.sh
@@ -70,10 +70,28 @@ pip3 install matplotlib numpy jupyterlab
 sudo apt install texlive-latex-extra texlive-full pandoc -y
 
 # Node.js Installation
-curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install gcc g++ make -y 
 sudo apt-get install nodejs -y
+
+# Docker
+sudo apt-get update -y
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
 # Java Installation
 sudo apt install openjdk-11-jdk -y
